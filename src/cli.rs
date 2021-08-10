dda0a273b66ef1d346469033df8248a2e0ba4b74
R <37774865+rchap4@users.noreply.github.com>
2021-08-09 20:02:43 -0600

    Added license to file

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,19 @@
+// Copyright RChapman 2021
+// This file is part of git-rusty, Rust examples for Git Operations.
+
+// git-rusty is free software: you can redistribute it and/or modify
+// it under the terms of the Affero GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// git-rusty is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Affero GNU General Public License for more details.
+
+// You should have received a copy of the Affero GNU General Public License
+// along with git-rusty.  If not, see <https://www.gnu.org/licenses/>.
+
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
